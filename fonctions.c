52060f9459ee3ec67fc6a131efa9748f2a8e8a5c
Paul Levesque <paul-antoine.levesque@lip6.fr>
2021-04-27 20:43:14 +0200

    update

--- fonctions.c
+++ fonctions.c
@@ -3,48 +3,48 @@
 
 int celsius_vers_kelvin(int temperature) {
 
-		int retour = temperature + 273;
-		return retour;
+    int retour = temperature + 273;
+    return retour;
 
 }
 
 int celsius_vers_fahrenheit(int temperature) {
 
-		float res = (9.0/5.0) * temperature + 32;
-		int retour = (int) (res);
-		return retour;
+    float res = (9.0/5.0) * temperature + 32;
+    int retour = (int) (res);
+    return retour;
 
 }
 
 int fahrenheit_vers_celsius(int temperature) {
 
-	float res = (temperature - 32)/1.8;
-	int retour = (int) (res);
-	return retour;
+    float res = (temperature - 32)/1.8;
+    int retour = (int) (res);
+    return retour;
 
 }
 
 
 int fahrenheit_vers_kelvin(int temperature) {
 
-		int tmp = fahrenheit_vers_celsius(temperature);
-		tmp = celsius_vers_kelvin(tmp);
-		return tmp;
+    int tmp = fahrenheit_vers_celsius(temperature);
+    tmp = celsius_vers_kelvin(tmp);
+    return tmp;
 
 }
 
 int kelvin_vers_celsius(int temperature) {
 
-		int retour = temperature - 273;
-		return retour;
+    int retour = temperature - 273;
+    return retour;
 
 }
 
 int kelvin_vers_fahrenheit(int temperature) {
 
-		int tmp = kelvin_vers_celsius(temperature);
-		tmp = celsius_vers_fahrenheit(tmp);
-		return tmp;
+    int tmp = kelvin_vers_celsius(temperature);
+    tmp = celsius_vers_fahrenheit(tmp);
+    return tmp;
 
 }
 
--- main.c
+++ main.c
@@ -3,117 +3,117 @@
 
 int main() {
 
-		int depart;
-		int arrivee;
-		int t;
+    int depart;
+    int arrivee;
+    int t;
 
-	do {
+    do {
 
-		printf("Choisissez l'unité de départ : \n\n");
-		printf("1. Celsius \n");
-		printf("2. Fahrenheit \n");
-		printf("3. Kelvin \n");
-		printf("4. Quitter \n");
-		printf("Entrez 1, 2 ou 3 selon l'unité souhaitée : \n");		
-		printf("(Entrez un nombre entier uniquement compris entre 1 et 4)\n\n");
-		scanf("%d", &depart);
+	printf("Choisissez l'unité de départ : \n\n");
+	printf("1. Celsius \n");
+	printf("2. Fahrenheit \n");
+	printf("3. Kelvin \n");
+	printf("4. Quitter \n");
+	printf("Entrez 1, 2 ou 3 selon l'unité souhaitée : \n");		
+	printf("(Entrez un nombre entier uniquement compris entre 1 et 4)\n\n");
+	scanf("%d", &depart);
 		
-		if(depart == 4) break;
+	if(depart == 4) break;
 
-		printf("Entrez la température que vous souhaitez convertir : \n");
-		printf("(Entrez un nombre entier uniquement)\n \n");
-		scanf("%d", &t);
+	printf("Entrez la température que vous souhaitez convertir : \n");
+	printf("(Entrez un nombre entier uniquement)\n \n");
+	scanf("%d", &t);
 
-		printf("Choisissez l'unité d'arrivée : \n\n");
-		printf("1. Celsius \n");
-		printf("2. Fahrenheit \n");
-		printf("3. Kelvin \n");
-		printf("4. Quitter \n");
-		printf("Entrez 1, 2 ou 3 selon l'unité souhaitée : \n");		
-		printf("(Entrez un nombre entier uniquement compris entre 1 et 4)\n\n");
+	printf("Choisissez l'unité d'arrivée : \n\n");
+	printf("1. Celsius \n");
+	printf("2. Fahrenheit \n");
+	printf("3. Kelvin \n");
+	printf("4. Quitter \n");
+	printf("Entrez 1, 2 ou 3 selon l'unité souhaitée : \n");		
+	printf("(Entrez un nombre entier uniquement compris entre 1 et 4)\n\n");
 	
-		scanf("%d", &arrivee);
+	scanf("%d", &arrivee);
 
-		if(arrivee == 4) break;
+	if(arrivee == 4) break;
 
-		//on fait tous les cas possibles en sachant que ça ne peut changer qu'une fois
+	//on fait tous les cas possibles en sachant que ça ne peut changer qu'une fois
 
 
-		//on traite les cas simples en premier
-		if(depart == 1 && arrivee == 1) { 
+	//on traite les cas simples en premier
+	if(depart == 1 && arrivee == 1) { 
 
-				printf("La température est de %d degrés Celsius.\n\n", t);
+	    printf("La température est de %d degrés Celsius.\n\n", t);
 
-		}
+	}
 
-		if(depart == 2 && arrivee == 2) {
+	if(depart == 2 && arrivee == 2) {
 			
-				printf("La température est de %d degrés Fahrenheit.\n\n", t);
+	    printf("La température est de %d degrés Fahrenheit.\n\n", t);
 
-		}
+	}
 
-		if(depart == 3 && arrivee == 3) {
+	if(depart == 3 && arrivee == 3) {
 			
-				printf("La température est de %d degrés Kelvin.\n\n", t);
+	    printf("La température est de %d degrés Kelvin.\n\n", t);
 
-		}
+	}
 
-		//celsius vers autre chose
-		if(depart == 1 && arrivee == 2) {
+	//celsius vers autre chose
+	if(depart == 1 && arrivee == 2) {
 
-				printf("La température entrée est de %d degrés Celsius.\n\n", t);
-				t = celsius_vers_fahrenheit(t);
-				printf("La température obtenue est de %d degrés Fahrenheit.\n\n", t);
+	    printf("La température entrée est de %d degrés Celsius.\n\n", t);
+	    t = celsius_vers_fahrenheit(t);
+	    printf("La température obtenue est de %d degrés Fahrenheit.\n\n", t);
 				
-		}
+	}
 
-		if(depart == 1 && arrivee == 3) {
+	if(depart == 1 && arrivee == 3) {
 
-				printf("La température entrée est de %d degrés Celsius.\n\n", t);
-				t = celsius_vers_kelvin(t);
-				printf("La température obtenue est de %d degrés Kelvin.\n\n", t);
+	    printf("La température entrée est de %d degrés Celsius.\n\n", t);
+	    t = celsius_vers_kelvin(t);
+	    printf("La température obtenue est de %d degrés Kelvin.\n\n", t);
 
-		}
+	}
 
-		//fahrenheit vers autre chose
-		if(depart == 2 && arrivee == 1) {
+	//fahrenheit vers autre chose
+	if(depart == 2 && arrivee == 1) {
 
-				printf("La température entrée est de %d degrés Fahrenheit.\n\n", t);
-				t = fahrenheit_vers_celsius(t);
-				printf("La température obtenue est de %d degrés Celsius.\n\n", t);
+	    printf("La température entrée est de %d degrés Fahrenheit.\n\n", t);
+	    t = fahrenheit_vers_celsius(t);
+	    printf("La température obtenue est de %d degrés Celsius.\n\n", t);
 				
-		}
+	}
 
-		if(depart == 2 && arrivee == 3) {
+	if(depart == 2 && arrivee == 3) {
 
-				printf("La température entrée est de %d degrés Fahrenheit.\n\n", t);
-				t = fahrenheit_vers_kelvin(t);
-				printf("La température obtenue est de %d degrés Kelvin.\n\n", t);
+	    printf("La température entrée est de %d degrés Fahrenheit.\n\n", t);
+	    t = fahrenheit_vers_kelvin(t);
+	    printf("La température obtenue est de %d degrés Kelvin.\n\n", t);
 
-		}
+	}
 
-		//kelvin vers autre chose
-		if(depart == 3 && arrivee == 1) {
+	//kelvin vers autre chose
+	if(depart == 3 && arrivee == 1) {
 
-				printf("La température entrée est de %d degrés Kelvin.\n\n", t);
-				t = kelvin_vers_celsius(t);
-				printf("La température obtenue est de %d degrés Celsius.\n\n", t);
+	    printf("La température entrée est de %d degrés Kelvin.\n\n", t);
+	    t = kelvin_vers_celsius(t);
+	    printf("La température obtenue est de %d degrés Celsius.\n\n", t);
 				
-		}
+	}
 
-		if(depart == 3 && arrivee == 2) {
+	if(depart == 3 && arrivee == 2) {
 
-				printf("La température entrée est de %d degrés Kelvin.\n\n", t);
-				t = kelvin_vers_fahrenheit(t);
-				printf("La température obtenue est de %d degrés Fahrenheit.\n\n", t);
+	    printf("La température entrée est de %d degrés Kelvin.\n\n", t);
+	    t = kelvin_vers_fahrenheit(t);
+	    printf("La température obtenue est de %d degrés Fahrenheit.\n\n", t);
 
-		}
+	}
 
-	} while (1);
+    } while (1);
 
-	printf("Relancez le programme pour convertir de nouveau d'autres températures.\n\n");
+    printf("Relancez le programme pour convertir de nouveau d'autres températures.\n\n");
 
-	return 0;
+    return 0;
 
 }
 
